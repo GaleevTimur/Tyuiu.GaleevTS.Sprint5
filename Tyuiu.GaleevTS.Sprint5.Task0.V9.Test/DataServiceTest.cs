067ee7f47218fcc8f6c3a88b387ca3906e01668a
GaleevTimur <timur.galeev.05@yandex.ru>
2023-11-19 22:08:46 +0500

    Add project files.

---
+++ Tyuiu.GaleevTS.Sprint5.Task0.V9.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GaleevTS.Sprint5.Task0.V9.Lib;
+using System.IO;
+
+namespace Tyuiu.GaleevTS.Sprint5.Task0.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\timur_8n182p8\source\repos\Tyuiu.GaleevTS.Sprint5\Tyuiu.GaleevTS.Sprint5.Task0.V9\bin\Debug\OutPutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
+        }
+    }
+}
---
+++ Tyuiu.GaleevTS.Sprint5.Task0.V9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GaleevTS.Sprint5.Task0.V9.Lib;
+using System.IO;
+
+
+namespace Tyuiu.GaleevTS.Sprint5.Task0.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            DataService ds = new DataService();
+            Console.Title = "Спринт №5 | Выполнил: Галеев Т. С. | ИИПб-23-3";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                *");
+            Console.WriteLine("* Тема: Двумерные массивы. (генератор случайных чисел)                     *");
+            Console.WriteLine("* Задание №0                                                               *");
+            Console.WriteLine("* Вариант #9                                                               *");
+            Console.WriteLine("* Выполнил: Галеев Тимур Серикович | ИИПб-23-3                             *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл:" + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
