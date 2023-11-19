ac6c85118fb8464f9ff49dbc14ea9fc6789285e1
GaleevTimur <timur.galeev.05@yandex.ru>
2023-11-19 22:20:21 +0500

    Task0

--- Tyuiu.GaleevTS.Sprint5.Task0.V9/Program.cs
+++ Tyuiu.GaleevTS.Sprint5.Task0.V9/Program.cs
@@ -18,7 +18,7 @@ namespace Tyuiu.GaleevTS.Sprint5.Task0.V9
             Console.Title = "Спринт №5 | Выполнил: Галеев Т. С. | ИИПб-23-3";
             Console.WriteLine("****************************************************************************");
             Console.WriteLine("* Спринт #5                                                                *");
-            Console.WriteLine("* Тема: Двумерные массивы. (генератор случайных чисел)                     *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                         *");
             Console.WriteLine("* Задание №0                                                               *");
             Console.WriteLine("* Вариант #9                                                               *");
             Console.WriteLine("* Выполнил: Галеев Тимур Серикович | ИИПб-23-3                             *");
