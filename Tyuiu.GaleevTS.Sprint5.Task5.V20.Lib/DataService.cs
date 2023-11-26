b4adbd6690003af0e8a30ae81ad612e0c4637683
GaleevTimur <timur.galeev.05@yandex.ru>
2023-11-26 21:26:37 +0500

    Task5

---
+++ Tyuiu.GaleevTS.Sprint5.Task5.V20.Lib/DataService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.GaleevTS.Sprint5.Task5.V20.Lib
+{
+    public class DataService : ISprint5Task5V20
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 0;
+            int count = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    double x = Convert.ToDouble(line);
+                    if (x >= -10)
+                    {
+                        if (x <= 10)
+                        {
+                            count++;
+                            res = res + Convert.ToDouble(line);
+                        }
+                    }
+
+
+                }
+            }
+            res = res / count;
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.GaleevTS.Sprint5.Task5.V20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.GaleevTS.Sprint5.Task5.V20.Lib;
+
+namespace Tyuiu.GaleevTS.Sprint5.Task5.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт №5 | Выполнил: Галеев Т. С. | ИИПб-23-3";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                  *");
+            Console.WriteLine("* Задание №5                                                               *");
+            Console.WriteLine("* Вариант #20                                                              *");
+            Console.WriteLine("* Выполнил: Галеев Тимур Серикович | ИИПб-23-3                             *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                          *");
+            Console.WriteLine("****************************************************************************");
+            string path = @"C:\Users\timur_8n182p8\source\repos\Tyuiu.GaleevTS.Sprint5\Tyuiu.GaleevTS.Sprint5.Task5.V20\bin\Debug\InPutDataFileTask5V20.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Среднее значение чисел в промежутке от -10 до 10 в файле" + res);
+            Console.ReadKey();
+        }
+    }
+}
