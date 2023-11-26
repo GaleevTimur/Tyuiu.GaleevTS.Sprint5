735445ea4499d52af0d5e6e7a2a23694d5954a07
GaleevTimur <timur.galeev.05@yandex.ru>
2023-11-26 22:07:25 +0500

    Task6

--- Tyuiu.GaleevTS.Sprint5.Task5.V20/Program.cs
+++ Tyuiu.GaleevTS.Sprint5.Task5.V20/Program.cs
@@ -16,8 +16,8 @@ namespace Tyuiu.GaleevTS.Sprint5.Task5.V20
             Console.Title = "Спринт №5 | Выполнил: Галеев Т. С. | ИИПб-23-3";
             Console.WriteLine("****************************************************************************");
             Console.WriteLine("* Спринт #5                                                                *");
-            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                           *");
-            Console.WriteLine("* Задание №5                                                               *");
+            Console.WriteLine("* Тема: Обработка текстовых файлов                                         *");
+            Console.WriteLine("* Задание №6                                                               *");
             Console.WriteLine("* Вариант #20                                                              *");
             Console.WriteLine("* Выполнил: Галеев Тимур Серикович | ИИПб-23-3                             *");
             Console.WriteLine("****************************************************************************");
